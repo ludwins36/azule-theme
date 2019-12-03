d9a91e2f6ded7c611d9c3a4c91c1fc63a877fd80
azuleServer <ludwins36@gmail.com>
2019-12-03 19:20:59 +0100

    guardando tema clasic de azule para modificar

---
+++ mails/pe/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Your order has been changed'] = '';
+$_LANGMAIL['Product out of stock'] = '';
+$_LANGMAIL['Request to activate the seller'] = '';
+$_LANGMAIL['The seller added a new product'] = '';
+$_LANGMAIL['Congratulations you have sold your product'] = '';
+$_LANGMAIL['Withdrawal of rewards ordered'] = '';
+$_LANGMAIL['Stock coverage'] = '';
+$_LANGMAIL['New return from order #%d - %s'] = '';
+$_LANGMAIL['New order : #%d - %s'] = '';
+$_LANGMAIL['Product available'] = '';
+
+?>
